c0ea6323a30821ef35f69d10138c4af655362727
japerry911 <jack.e.perry@outlook.com>
2020-09-09 16:04:17 -0600

    first basic graphql schema and connection built and tested

---
+++ backend/resolvers.js
@@ -0,0 +1,9 @@
+const careersQuery = () => {
+  return [{ id: 1, title: "poop", location: "USA", appLink: "link.com" }];
+};
+
+const Query = {
+  careers: careersQuery,
+};
+
+module.exports = { Query };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { ApolloServer, gql } = require("apollo-server-express");
+const fs = require("fs");
+const resolvers = require("./resolvers");
+
+const port = 9000;
 
 const app = express();
+app.use(cors(), bodyParser.json());
 
-app.get("/", function (req, res) {
-  return res.send("Hello World");
+app.get("/api", (req, res) => {
+  res.send("HOORAY");
 });
 
-app.listen(process.env.PORT || 8000);
+const typeDefs = gql(fs.readFileSync("./schema.graphql", { encoding: "utf8" }));
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen(port, () => console.info(`Server started on port ${port}`));
